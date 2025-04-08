1d24b54b05fe00ff0c377497697008972cb88187
Harlan Williams <hrw@ku.edu>
2025-04-08 10:07:36 -0500

    add comment

--- calculator.hpp
+++ calculator.hpp
@@ -3,6 +3,8 @@
 
 #include <string>
 
+// this function should expect that `expression` has already been
+// validated and thus throw no exceptions
 double parse_number(const std::string &expression);
 
 #endif // __CALCULATOR_HPP
