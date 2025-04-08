2a13588d37ff810e97cc086c9b1facc777539947
Harlan Williams <hrw@ku.edu>
2025-04-07 22:20:56 -0500

    try something

--- calculator.hpp
+++ calculator.hpp
@@ -1,6 +1,16 @@
 #ifndef __CALCULATOR_HPP
 #define __CALCULATOR_HPP
 
+#include <memory>
+#include <string>
 
+class Expression {
+public:
+    virtual double evaluate() = 0;
+};
+
+using ExprPtr = std::shared_ptr<Expression>;
+
+ExprPtr parse_expression(const std::string &expression);
 
 #endif // __CALCULATOR_HPP
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,5 +3,6 @@
 #include "calculator.hpp"
 
 TEST(CalculatorImplementation, PlaceholderTest) {
-
+    auto expr = parse_expression("1");
+    EXPECT_NEAR(expr->evaluate(), 1.0, 0.001);
 }
